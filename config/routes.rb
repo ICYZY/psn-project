e6219714e952cca5d2b2a0b78f65285f3545a920
Pantakan Chusri-eam <shamanking_yoh_anna@hotmail.com>
2017-04-10 07:09:06 +0000

    Added product home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+  get 'products/index'
+
   devise_for :users, :controllers =>{ :omniauth_callbacks => "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' , to: 'psntest#psn', as: 'root'
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create! id: 1, name: "10$", price: 350, image: "psn10us.jpg", category: "Sony", active: true
+Product.create! id: 2, name: "20$", price: 700, image: "psn20us.jpg", category: "Sony", active: true
+Product.create! id: 3, name: "50$", price: 1750, image: "psn50us.jpg", category: "Sony", active: true
+Product.create! id: 4, name: "60$", price: 2100, image: "psn60us.jpg", category: "Sony", active: true
+Product.create! id: 5, name: "75$", price: 2625, image: "psn75us.jpg", category: "Sony", active: true
+Product.create! id: 6, name: "100$", price: 3500, image: "psn100us.jpg", category: "Sony", active: true
+
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
